ba68479144431dcf398952c9048b53dd75e6f876
IvanDarmovis <ivan.darmovis1@gmail.com>
2022-04-09 13:44:24 +0300

    conflict solved

--- src/js/refs.js
+++ src/js/refs.js
@@ -2,5 +2,7 @@ const refs = {
   searchBtn: document.querySelector('.search-form'),
   libraryBtn: document.querySelector('header'),
   filmContainer: document.querySelector('.thumb'),
+  galleryMovies: document.querySelector('.gallery-movies'),
 };
+
 export default refs;
--- src/js/searchMovie.js
+++ src/js/searchMovie.js
@@ -1,12 +1,23 @@
 import refs from './refs';
 import ApiService from './api-service';
+import makeMovieMarkup from './moviesMarkup';
 
 const api = new ApiService();
 
-refs.searchBtn.addEventListener('submit', searchMovie);
+refs.searchBtn.addEventListener('submit', getMovie);
 
-function searchMovie(e) {
+function getMovie(e) {
   e.preventDefault();
   api.query = e.currentTarget.elements.searchQuery.value;
-  api.fetchMovieBySearch();
+  api.fetchMovieBySearch().then(data => {
+    console.log(data);
+    renderMovies(data);
+  });
+  // .catch(err => handleError(err));
+}
+
+function renderMovies(data) {
+  const markup = makeMovieMarkup(data.results);
+  console.log(markup);
+  refs.galleryMovies.innerHTML = markup;
 }
