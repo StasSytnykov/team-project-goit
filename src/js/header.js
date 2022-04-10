b3441c0b359b0a3d0a3eb3cc71e371d61bac2161
DimaDovgan <89789155+DimaDovgan@users.noreply.github.com>
2022-04-10 16:23:46 +0300

    add some

--- src/js/header.js
+++ src/js/header.js
@@ -1,26 +1,28 @@
+import refs from './refs';
 
-myRefs = {
-    library: document.querySelector(".library"),
-    home: document.querySelector(".home"),
-    spanHome: document.querySelector(".span-home"),
-    spanLibrary: document.querySelector(".span-library")
-
-}
-
+const containerHeader = document.querySelector(".container");
 const libraryClick = (Event) => {
     Event.preventDefault();
-    myRefs.spanHome.style.display = "none";
-    myRefs.spanLibrary.style.display = "block";
+    refs.spanHome.style.display = "none";
+    refs.spanLibrary.style.display = "block";
+    refs.searchBtn.style.display = "none";
+    refs.libraryBtnList.classList.remove("not-displayed");
+    refs.libraryBtnList.classList.add("button-list");
+    
+    containerHeader.style.backgroundImage = "url('../images/header/Header-library.jpg')";
     
 }
 const homeClick = (Event) => {
     Event.preventDefault();
-    myRefs.spanLibrary.style.display = "none";
-    myRefs.spanHome.style.display = "block";
+    refs.spanLibrary.style.display = "none";
+    refs.spanHome.style.display = "block";
+    refs.libraryBtnList.classList.remove("button-list");
+    refs.libraryBtnList.classList.add("not-displayed");
+    refs.searchBtn.style.display = "block";
+    containerHeader.style.backgroundImage = "url('../images/header/header-mobile-bcg.jpg')";
     
 }
 
-myRefs.library.addEventListener("click", libraryClick);
-myRefs.home.addEventListener("click", homeClick);
-
+refs.libraryLi.addEventListener("click", libraryClick);
+refs.homeLi.addEventListener("click", homeClick);
 
