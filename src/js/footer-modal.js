64d553eea02931869878494d15810d2faa8295e8
Stas Sytnykov <stas82kak05@gmail.com>
2022-04-13 17:09:09 +0300

    add logic on modal

--- src/js/footer-modal.js
+++ src/js/footer-modal.js
@@ -7,11 +7,26 @@ const modalCloseBtn = document.querySelector('.footer-modal__close-btn');
 const onOpenFooterModal = event => {
   event.preventDefault();
   footerBackdrop.classList.remove('is-hidden');
+  window.addEventListener('keydown', onCloseFooterModalOnEsc);
 };
 
 const onCloseFooterModal = () => {
-  backdrop.classList.add('is-hidden');
+  footerBackdrop.classList.add('is-hidden');
+  window.removeEventListener('keydown', onCloseFooterModalOnEsc);
 };
 
+const onCloseModalOnBackdropClick = event => {
+  if (event.currentTarget === event.target) {
+    footerBackdrop.classList.add('is-hidden');
+  }
+};
+
+function onCloseFooterModalOnEsc(event) {
+  if (event.code === 'Escape') {
+    onCloseFooterModal();
+  }
+}
+
+footerBackdrop.addEventListener('click', onCloseModalOnBackdropClick);
 modalCloseBtn.addEventListener('click', onCloseFooterModal);
 footerLink.addEventListener('click', onOpenFooterModal);
