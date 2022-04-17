5f846b105b25cbd91c44524c5fad2be43a806aac
zpengineer <clickprime94@gmail.com>
2022-04-17 18:29:42 +0300

    Fixed theme

--- src/js/moon-sun.js
+++ src/js/moon-sun.js
@@ -3,6 +3,34 @@ const chk = document.getElementById('chk');
 
 
 chk.addEventListener('change', () => {
-    document.body.classList.toggle('dark');
+    
+
+    if (localStorage.getItem('theme') === 'dark') {
+        localStorage.removeItem('theme');
+    }
+    else {
+        localStorage.setItem('theme', 'dark');
+    }
+
+    addDarkTheme();
    
 });
+
+function addDarkTheme() {
+    try {
+
+        if (localStorage.getItem('theme') === 'dark') {
+
+            document.body.classList.add('dark');
+            // chk.classList.add('chexkbox:checked');
+            chk.checked = true;
+        }
+        else {
+            document.body.classList.remove('dark');
+            chk.checked = false;
+        }
+        
+    }catch(err) { }
+}
+
+addDarkTheme();
