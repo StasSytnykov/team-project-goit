b3b42e1bd265d892ceeae7c2d7590ffb51141ab5
Drummberg <drummberg@gmail.com>
2022-04-18 00:24:10 +0300

    Update moon-sun.js

--- src/js/moon-sun.js
+++ src/js/moon-sun.js
@@ -1,3 +1,4 @@
+import refs from './refs'
 
 const chk = document.getElementById('chk');
 
@@ -20,13 +21,17 @@ function addDarkTheme() {
     try {
 
         if (localStorage.getItem('theme') === 'dark') {
-
             document.body.classList.add('dark');
             chk.checked = true;
+            refs.modal.classList.add('dark')
+            refs.modalInfo.classList.add('dark')
+            
         }
         else {
             document.body.classList.remove('dark');
+            refs.modal.classList.remove('dark')
             chk.checked = false;
+            refs.modalInfo.classList.remove('dark')
         }
         
     }catch(err) { }
