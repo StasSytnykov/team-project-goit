046295dcccfbd164c57255b7244afa3e9ef43d52
Stas Sytnykov <stas82kak05@gmail.com>
2022-04-20 17:15:00 +0300

    fix footer fucn and render home page func

--- src/js/render-home-page.js
+++ src/js/render-home-page.js
@@ -29,7 +29,7 @@ const onClickHomeBtn = event => {
   // document.removeEventListener('scroll', infiniteScroll, true);
   // fetchPopularFilms();
   temp();
-  // moviePagination.firstPage();
+  moviePagination.firstPage();
 };
 
 refs.homeLi.addEventListener('click', onClickHomeBtn);
